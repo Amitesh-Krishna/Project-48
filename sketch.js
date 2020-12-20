59914ede6452b8f8569a7b035b82a8cfd8e72bff
Amitesh Krishna <anantk124@gmail.com>
2020-12-20 19:18:19 +0530

    Project-46 updated

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-
 var birdy;
+var obs = [];
+var gameState = 0;
 
 function setup() {
 	createCanvas(800, 800);
@@ -11,14 +12,32 @@ function setup() {
 function draw() {
 	background(0,255,255);
 
-	Move();
+	if(gameState == 0){
+		//Game playing
 
+		Move();
+		createObstacles();
+
+		for(var i = 0;i<obs.length;i++){
+			if(birdy.isTouching(obs[i])){
+				gameState = 1;
+			}
+		}
+	}
+	else if(gameState == 1){
+
+		birdy.velocityX = 0;
+		birdy.velocityY = 10;
+	}
+	else if(gameState == 2){
+		// Game won
+	}
 	drawSprites();
 }
 
 function Move(){
-	birdy.velocityX = 2;
-	camera.x += 2;
+	birdy.velocityX = frameCount/100;
+	camera.x += frameCount/100;
 
 	if(keyWentDown(32)){
 		birdy.velocityY = -10;
@@ -27,3 +46,18 @@ function Move(){
 	birdy.velocityY += 0.4;
 }
 
+function createObstacles(){
+	if(frameCount % 100 == 0){
+		var obsY = random(0,250);
+
+		var obsUp = createSprite(birdy.x + 750,obsY,50,500);
+		obsUp.lifetime = 400;
+		obsUp.shapeColor = "lime";
+		var obsDown = createSprite(birdy.x + 750,obsY + 650,50,500);
+		obsDown.lifetime = 400;
+		obsDown.shapeColor = "lime";
+
+		obs.push(obsUp);
+		obs.push(obsDown);
+	}
+}
